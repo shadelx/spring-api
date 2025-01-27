e8c193c7318fa4160f0300aa82dd93b40c83d2c5
shadelx <shadelx.07@gmail.com>
2025-01-27 12:27:32 -0600

    feat: add new file structure

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,4 +1,4 @@
-package com.example.demo.student;
+package com.example.demo.controller;
 
 import java.util.List;
 
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
 
 @RestController
 @RequestMapping(path = "api/students")
--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,4 +1,4 @@
-package com.example.demo.student;
+package com.example.demo.model;
 
 import java.time.LocalDate;
 
